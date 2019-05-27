ea73b1fd76857a694ca309b6f48334d266e2d068
Robinson Muñoz <32588323+Dumaru@users.noreply.github.com>
2019-05-26 22:12:50 -0500

    Codigo 6x6 y mensajes en serial

--- Arduino/OvoGame/OvoGame.ino
+++ Arduino/OvoGame/OvoGame.ino
@@ -3,12 +3,12 @@
   https://www.baldengineer.com/tips/arduino-pinmode-on-analog-inputs
 */
 
-const byte tamanoTablero = 4;
+const byte tamanoTablero = 6;
 const int entradas[tamanoTablero][tamanoTablero] = {
-  {0, 2, 3, 4},
-  {5, 6, 7, 8},
-  {9, 10, 11, 12},
-  {13, 14, 15, 16}
+  {2, 3, 4, 5, 6, 7},
+  {8, 9, 10, 11, 12, 13},
+  {22, 23, 24, 25, 26, 27},
+  {28, 29, 30, 31, 32, 33}
 };
 
 // La RX no pasa nada PERO con la TX se jode por que se intenta
@@ -21,7 +21,7 @@ const int entradas[tamanoTablero][tamanoTablero] = {
 byte tablero[tamanoTablero][tamanoTablero];
 byte cantidadJugadores = 0;
 boolean enJuego = false;
-byte areaBusqueda = 1;
+byte areaBusqueda = 2;
 
 byte partidaI = 10;
 byte partidaJ = 10;
@@ -40,23 +40,29 @@ void setup()
 
 void loop()
 {
-  Serial.println(cantidadJugadores);
+  //Serial.println(cantidadJugadores);
   if (cantidadJugadores == 0) {
     // Me puede regresar solo 2 o 4  0 para cuando no se halla seteado el juego
     cantidadJugadores = identificarJugadores();
-    Serial.print("La cantidad de jugadores es: ");
-    Serial.println(cantidadJugadores);
 
+    /*
+      Serial.print("La cantidad de jugadores es: ");
+      Serial.println(cantidadJugadores);
+    */
     //enJuego = cantidadJugadores != 0 ? true : false;
     if (cantidadJugadores != 0) {
       enJuego = true;
       configurarTablero(cantidadJugadores);
+      String mensajeInicio = "INICIO:";
+      mensajeInicio.concat(cantidadJugadores);
+      Serial.println(mensajeInicio);
+      delay(1000);
     }
 
   }
   if (enJuego) {
     // Se comenzo/esta juegando
-    Serial.println(" Se inicia ya que se ocuparon los puestos");
+    //Serial.println(" Se inicia ya que se ocuparon los puestos");
 
     // Registrar movimiento
     registroMovimiento();
@@ -67,7 +73,7 @@ void loop()
 
   }
 
-  imprimirTablero();
+  //imprimirTablero();
 
   //imprimirValores();
 }
@@ -104,23 +110,34 @@ void identificarLlegada() {
         if ( tablero[i][j] == 0 && digitalRead(entradas[i][j]) == HIGH) {
           // A donde llego
 
+          // Actualizar tablero
           tablero[i][j] = 1;
-
           tablero[partidaI][partidaJ] = 0;
+
           finI = i;
           finJ = j;
 
-          Serial.print("Partida i ");
-          Serial.print(partidaI);
-          Serial.print(", j ");
-          Serial.print(partidaJ);
-          Serial.println("------");
-
-          Serial.print("fin i ");
-          Serial.print(finI);
-          Serial.print(", j ");
-          Serial.print(finJ);
-          Serial.println("------");
+          String mensajeMovimiento = "MOVIMIENTO:";
+          mensajeMovimiento.concat(partidaI);
+          mensajeMovimiento.concat(",");
+          mensajeMovimiento.concat(partidaJ);
+          mensajeMovimiento.concat(":");
+          mensajeMovimiento.concat(finI);
+          mensajeMovimiento.concat(",");
+          mensajeMovimiento.concat(finJ);
+          Serial.println(mensajeMovimiento);
+          delay(5000);
+          //          Serial.print("Partida i ");
+          //          Serial.print(partidaI);
+          //          Serial.print(", j ");
+          //          Serial.print(partidaJ);
+          //          Serial.println("------");
+          //
+          //          Serial.print("fin i ");
+          //          Serial.print(finI);
+          //          Serial.print(", j ");
+          //          Serial.print(finJ);
+          //          Serial.println("------");
           break;
         }
       }
@@ -138,31 +155,27 @@ byte identificarJugadores() {
   byte num = 0;
   // Observar si solo son dos jugadores
   int suma = 0;
-  boolean listones = false;
 
   for (byte j  = 0; j < tamanoTablero; j++ ) {
-    delay(10);
     suma += digitalRead(entradas[0][j]);  // Primeros 6 botones, entradas es un multiarray
-    delay(10);
     suma += digitalRead(entradas[tamanoTablero - 1][j]); // Ultimos 6 botones, entradas es un multiarray
-    delay(10);
   }
-  Serial.print("Valor de la suma ");
-  Serial.println( suma );
+  //  Serial.print("Valor de la suma ");
+  //  Serial.println( suma );
   //suma = 8;
   //delay(6000);
   if (suma == tamanoTablero * 2) {
     num = 2; // Solo son 2 jugadores
-    listones = true;
-  } else if(!listones) {
+
+  } else {
     suma = 0;
     // Chequear los 4
     byte sj0 = posJugadores(0, 0);
-    byte sj1 = posJugadores(0, tamanoTablero-areaBusqueda);
-    byte sj2 = posJugadores(tamanoTablero-areaBusqueda, 0);
-    byte sj3 = posJugadores(tamanoTablero-areaBusqueda, tamanoTablero-areaBusqueda);
+    byte sj1 = posJugadores(0, tamanoTablero - areaBusqueda);
+    byte sj2 = posJugadores(tamanoTablero - areaBusqueda, 0);
+    byte sj3 = posJugadores(tamanoTablero - areaBusqueda, tamanoTablero - areaBusqueda);
     suma = sj0 + sj1 + sj2 + sj3;
-    num = (suma == 4 || suma == 16)? 4 : 0;
+    num = (suma == 16) ? 4 : 0;
   }
 
   return num;
@@ -219,13 +232,25 @@ void configurarTablero(byte jugadores) {
   } else {
 
     tablero[0][0] = 1;
+    tablero[0][1] = 1;
+    tablero[1][0] = 1;
+    tablero[1][1] = 1;
 
-    tablero[0][tamanoTablero - 1] = 1;
+    tablero[0][4] = 1;
+    tablero[0][5] = 1;
+    tablero[1][4] = 1;
+    tablero[1][5] = 1;
 
-    tablero[tamanoTablero - 1][0] = 1;
+    tablero[4][0] = 1;
+    tablero[4][1] = 1;
+    tablero[5][0] = 1;
+    tablero[5][1] = 1;
 
-    tablero[tamanoTablero - 1][tamanoTablero - 1] = 1;
-
+    tablero[4][4] = 1;
+    tablero[4][5] = 1;
+    tablero[5][4] = 1;
+    tablero[5][5] = 1;
+    
   }
 
 }
--- Arduino/tinkercad.ino
+++ Arduino/tinkercad.ino
@@ -1,99 +1,88 @@
 /*
-	Vim Stuff: https://linuxhandbook.com/basic-vim-commands/
-	4X4 SIMLATION ON TINKERCAD: https://www.tinkercad.com/things/6dSU9mU8tI3-proyectov4/editel
-	A0 instead of 14
-	https://www.baldengineer.com/tips/arduino-pinmode-on-analog-inputs
+  A0 instead of 14
+  https://www.baldengineer.com/tips/arduino-pinmode-on-analog-inputs
 */
-String jugadoresLetras = "ABCD";
+
 const byte tamanoTablero = 4;
 const int entradas[tamanoTablero][tamanoTablero] = {
-                                                   {0, 2, 3, 4},
-                                                   {5, 6, 7, 8},
-                                                   {9, 10, 11, 12},
-                                                   {13, 14, 15, 16}
-                                                 };
+  {0, 2, 3, 4},
+  {5, 6, 7, 8},
+  {9, 10, 11, 12},
+  {13, 14, 15, 16}
+};
 
 // La RX no pasa nada PERO con la TX se jode por que se intenta
 // comunicar
 
 /*
   Para 2 jugadores seran jugador A y B, Para 4 seran jugador A,B,C Y D.
-  
 */
 
-String tablero[tamanoTablero][tamanoTablero];
+byte tablero[tamanoTablero][tamanoTablero];
 byte cantidadJugadores = 0;
 boolean enJuego = false;
+byte areaBusqueda = 2;
 
-
-byte partidaI;
-byte partidaJ;
+byte partidaI = 10;
+byte partidaJ = 10;
 byte finI;
 byte finJ;
 
 void setup()
 {
   Serial.begin(9600);
-  for(byte i = 0; i < tamanoTablero; i++){
-    for(byte j = 0; j < tamanoTablero; j++){
+  for (byte i = 0; i < tamanoTablero; i++) {
+    for (byte j = 0; j < tamanoTablero; j++) {
       pinMode(entradas[i][j], INPUT);
     }
   }
 }
 
 void loop()
-{	
+{
   Serial.println(cantidadJugadores);
-  if(cantidadJugadores == 0){
-       cantidadJugadores = identificarJugadores();
-       if(cantidadJugadores == 2){
-         // Configurar para dos
-         configurarTablero(2);
-         enJuego = true;
-       }else if(cantidadJugadores == 4){
-         // Configurar para 4
-         configurarTablero(4);
-         enJuego = true;
-       }
-    
-  }
-  if(enJuego){
-    // Se comenzo/esta juegando
-    Serial.println("Se inicia ya que se ocuparon los puestos");
-    
-    for(byte i = 0; i < cantidadJugadores; i++){
-      // Tomar turno para el jugador 
-      Serial.print("El jugador ");
-      Serial.println(jugadoresLetras[i]);
-      
-      // Registrar movimiento
-      registroMovimiento();
-      delay(1000);
-      identificarLlegada();
-      
-      partidaI = 10;
-      partidaJ = 10;
-      delay(3000);
+  if (cantidadJugadores == 0) {
+    // Me puede regresar solo 2 o 4  0 para cuando no se halla seteado el juego
+    cantidadJugadores = identificarJugadores();
+    Serial.print("La cantidad de jugadores es: ");
+    Serial.println(cantidadJugadores);
+
+    //enJuego = cantidadJugadores != 0 ? true : false;
+    if (cantidadJugadores != 0) {
+      enJuego = true;
+      configurarTablero(cantidadJugadores);
     }
-      
+
   }
-   
-   imprimirTablero();
-   
-   //imprimirValores();
+  if (enJuego) {
+    // Se comenzo/esta juegando
+    Serial.println(" Se inicia ya que se ocuparon los puestos");
+
+    // Registrar movimiento
+    registroMovimiento();
+    delay(1000);
+    identificarLlegada();
+
+    delay(3000);
+
+  }
+
+  imprimirTablero();
+
+  //imprimirValores();
 }
 
 /*
   Detectar el movimiento del huevo, comparando los valores en las posiciones dependiendo del turno
   del jugador
-  
+
 */
-void registroMovimiento(){
+void registroMovimiento() {
   // Conocer partida
-  for(byte i = 0; i < tamanoTablero; i++){
-    for(byte j = 0; j < tamanoTablero; j++){
+  for (byte i = 0; i < tamanoTablero; i++) {
+    for (byte j = 0; j < tamanoTablero; j++) {
       // En la posicion electronica se pasa a cero entonces veo lo que habia antes
-      if(tablero[i][j].length() > 0  && digitalRead(entradas[i][j]) == LOW){
+      if (tablero[i][j] == 1  && digitalRead(entradas[i][j]) == LOW) {
         // De donde se partio
         partidaI = i;
         partidaJ = j;
@@ -105,143 +94,134 @@ void registroMovimiento(){
 
 /*
   Identificacion de a donde se movio el huevo
-  
+
 */
-void identificarLlegada(){
-  if(partidaI != 10){
-      // Identificar fin
-    for(int i = 0; i < tamanoTablero; i++){
-      for(int j = 0; j < tamanoTablero; j++){
-        if( tablero[i][j].length() < 1 && digitalRead(entradas[i][j]) == HIGH){ 
+void identificarLlegada() {
+  if (partidaI != 10) {
+    // Identificar fin
+    for (int i = 0; i < tamanoTablero; i++) {
+      for (int j = 0; j < tamanoTablero; j++) {
+        if ( tablero[i][j] == 0 && digitalRead(entradas[i][j]) == HIGH) {
           // A donde llego
-          String nuevaP = tablero[partidaI][partidaJ] ;
-          Serial.println("Valor de partida");
-          Serial.println(nuevaP);
-          
-          
-          String iN = String(i);
-          String jN = String(j);
-          nuevaP.setCharAt(4, iN[0]);
-          Serial.print("Reemplazar el 4 ->");
-          Serial.println(nuevaP);
-          nuevaP.setCharAt(6, jN[0]);
-          Serial.print("Reemplazar el 6 ->");
-          Serial.println(nuevaP);
-          tablero[i][j] = nuevaP;
-          Serial.println("Valor de llegada cambiado");
-          Serial.println(nuevaP);
-          tablero[partidaI][partidaJ] = "";
+
+          tablero[i][j] = 1;
+
+          tablero[partidaI][partidaJ] = 0;
           finI = i;
           finJ = j;
-          
+
           Serial.print("Partida i ");
           Serial.print(partidaI);
           Serial.print(", j ");
           Serial.print(partidaJ);
           Serial.println("------");
-          
+
           Serial.print("fin i ");
           Serial.print(finI);
           Serial.print(", j ");
           Serial.print(finJ);
           Serial.println("------");
-                
-        
           break;
+        }
       }
     }
   }
-  
-  
-  }
-  
+
 }
 
-
-/*Inicializa el tablero de acuerdo a la cantidad de jugadores detectados al inicio*/
-void configurarTablero(byte jugadores){
-  if(jugadores == 2){
-    String strA = "A:I:0,";
-    String strB = "B:I:3,";
-    for(byte j = 0; j < tamanoTablero; j++){
-      tablero[0][j] = strA + j;
-      tablero[tamanoTablero-1][j] = strB + j;
-    }
-  }else{
-    String strA = "A:I:0,0";
-    String strB = "B:I:0,3";
-    String strC = "C:I:3,0";
-    String strD = "D:I:3,3";
-
-    tablero[0][0] = strA;
-
-    tablero[0][tamanoTablero-1] = strB;
-
-    tablero[tamanoTablero-1][0] = strC;
-
-    tablero[tamanoTablero-1][tamanoTablero-1] = strD;
-
-  }
-}
 
 /*
-  Funcion para identificar la cantidad de jugadores, viendo si en ciertas areas del tablero al iniciar se encuentran 
+  Funcion para identificar la cantidad de jugadores, viendo si en ciertas areas del tablero al iniciar se encuentran
   posicionandos los huevos.
 */
-byte identificarJugadores(){
+byte identificarJugadores() {
   byte num = 0;
   // Observar si solo son dos jugadores
   int suma = 0;
-  
-  for(byte j  = 0; j < tamanoTablero; j++ ){
-      suma += digitalRead(entradas[0][j]);  // Primeros 6 botones, entradas es un multiarray
-      suma += digitalRead(entradas[tamanoTablero-1][j]);  // Ultimos 6 botones, entradas es un multiarray 
+
+  for (byte j  = 0; j < tamanoTablero; j++ ) {
+    suma += digitalRead(entradas[0][j]);  // Primeros 6 botones, entradas es un multiarray
+    suma += digitalRead(entradas[tamanoTablero - 1][j]); // Ultimos 6 botones, entradas es un multiarray
   }
-  if(suma == tamanoTablero*2){
+  Serial.print("Valor de la suma ");
+  Serial.println( suma );
+  //suma = 8;
+  //delay(6000);
+  if (suma == tamanoTablero * 2) {
     num = 2; // Solo son 2 jugadores
-  }else{
-    // Chequear los 4 
-    num = 0;
-    if(digitalRead(entradas[0][0]) == HIGH && digitalRead(entradas[0][tamanoTablero-1]) == HIGH
-      && digitalRead(entradas[tamanoTablero-1][0]) == HIGH && digitalRead(entradas[tamanoTablero-1][tamanoTablero-1]) == HIGH
-      ){
-      num = 4;
-    }
+   
+  } else {
+    suma = 0;
+    // Chequear los 4
+    byte sj0 = posJugadores(0, 0);
+    byte sj1 = posJugadores(0, tamanoTablero-areaBusqueda);
+    byte sj2 = posJugadores(tamanoTablero-areaBusqueda, 0);
+    byte sj3 = posJugadores(tamanoTablero-areaBusqueda, tamanoTablero-areaBusqueda);
+    suma = sj0 + sj1 + sj2 + sj3;
+    num = (suma == 16)? 4 : 0;
   }
-    
+
   return num;
 }
 
+byte posJugadores(byte idxI, byte idxJ) {
+  byte suma = 0;
+  for (byte i = idxI; i < idxI + areaBusqueda; i++ ) {
+    for (byte j = idxJ; j < idxJ + areaBusqueda; j++) {
+      suma += digitalRead(entradas[i][j]);
+    }
+  }
+  return suma;
+}
 
-
-void imprimirValores(){
-  for(byte i = 0; i < tamanoTablero ; i++){
-    for(byte j = 0; j < tamanoTablero; j++){
+void imprimirValores() {
+  for (byte i = 0; i < tamanoTablero ; i++) {
+    for (byte j = 0; j < tamanoTablero; j++) {
       Serial.print("Valor fila ");
       Serial.print(i);
       Serial.print(" columna ");
       Serial.print(j);
-      
+
       Serial.print(" => ");
       Serial.println(digitalRead(entradas[i][j]));
       Serial.print("Final de carrera numero ");
-      
+
       delay(100);
     }
-  } 
+  }
 }
 
-void imprimirTablero(){
-  for(byte i = 0; i < tamanoTablero; i++){
-    for(byte j = 0; j < tamanoTablero; j++){
+void imprimirTablero() {
+  for (byte i = 0; i < tamanoTablero; i++) {
+    for (byte j = 0; j < tamanoTablero; j++) {
       Serial.print("Tablero Valor fila ");
       Serial.print(i);
       Serial.print(" columna ");
       Serial.print(j);
-      
+
       Serial.print(" => ");
       Serial.println(tablero[i][j]);
       delay(100);
     }
   }
+}
+
+void configurarTablero(byte jugadores) {
+  if (jugadores == 2) {
+    for (byte j = 0; j < tamanoTablero; j++) {
+      tablero[0][j] = 1;
+      tablero[tamanoTablero - 1][j] = 1;
+    }
+  } else {
+
+    tablero[0][0] = 1;
+
+    tablero[0][tamanoTablero - 1] = 1;
+
+    tablero[tamanoTablero - 1][0] = 1;
+
+    tablero[tamanoTablero - 1][tamanoTablero - 1] = 1;
+
+  }
+
 }
